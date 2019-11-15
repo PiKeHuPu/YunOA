54672f9b6ed0626d95209acace992e4c20b25a89
DTBinks <szl932832040@sina.com>
2019-11-15 11:44:26 +0800

    新增"车辆管理"系统

--- apps/oilWear/urls.py
+++ apps/oilWear/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^vehicle/manage', VehicleManageView.as_view(), name="vehicle_manage"),
     url(r'^vehicle/create', VehicleCreateView.as_view(), name="vehicle_create"),
     url(r'^vehicle/list', VehicleListView.as_view(), name="vehicle_list"),
+    url(r'^vehicle/adm', VehicleAdmView.as_view(), name="vehicle_adm"),
+    url(r'^vehicle/delete', VehicleDeleteView.as_view(), name="vehicle_delete"),
 ]
--- apps/oilWear/views.py
+++ apps/oilWear/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse
@@ -10,6 +11,8 @@ from rbac.models import Menu
 from system.models import SystemSetup
 from users.models import UserProfile
 from .models import Vehicle, Operator
+
+User = get_user_model()
 
 
 class VehicleManageView(LoginRequiredMixin, View):
@@ -58,10 +61,11 @@ class VehicleCreateView(LoginRequiredMixin, View):
         vehicle.creator_id = user_id
         vehicle.save()
 
-        operator = Operator()
-        operator.operator_id = request.POST.get('operator')
-        operator.vehicle_id = vehicle.id
-        operator.save()
+        if request.POST.get('operator'):
+            operator = Operator()
+            operator.operator_id = request.POST.get('operator')
+            operator.vehicle_id = vehicle.id
+            operator.save()
 
         res['result'] = True
         return HttpResponse(json.dumps(res), content_type='application/json')
@@ -74,17 +78,74 @@ class VehicleListView(LoginRequiredMixin, View):
 
     def get(self, request):
         fields = ['id', 'license_plate', 'department', 'type', 'create_time']
-        ret = dict(data=list(Vehicle.objects.values(*fields).order_by('id')))
+        ret = dict(data=list(Vehicle.objects.values(*fields).filter(is_delete=False).order_by('id')))
         # 获取载具管理员
         for car in ret['data']:
             operator_list = Operator.objects.filter(vehicle_id=car['id'])
             if operator_list:
                 operator = []
-                for i in operator_list:
-                    operator.append(i.operator.name)
-                operator = ",".join(operator)
-                car['operator'] = operator
+                if len(operator_list) <= 3:
+                    for i in operator_list:
+                        operator.append(i.operator.name)
+                    operator = ",".join(operator)
+                    car['operator'] = operator
+                else:
+                    for i in operator_list[:3]:
+                        operator.append(i.operator.name)
+                    operator = ",".join(operator)
+                    operator += " ..."
+                    car['operator'] = operator
             else:
                 car['operator'] = ""
 
         return HttpResponse(json.dumps(ret, cls=DjangoJSONEncoder), content_type='application/json')
+
+
+class VehicleAdmView(LoginRequiredMixin, View):
+    """
+    修改载具管理员
+    """
+    def get(self, request):
+        ret = dict()
+        if 'id' in request.GET and request.GET['id']:
+            vehicle = get_object_or_404(Vehicle, pk=int(request.GET.get('id')))
+            operator_obj_list = vehicle.operator_set.all()
+            operator_id_list = []
+            for operator in operator_obj_list:
+                operator_id_list.append(operator.operator_id)
+            added_adms = User.objects.filter(id__in=operator_id_list)
+            all_adms = User.objects.exclude(username='admin')
+            un_add_adms = set(all_adms).difference(added_adms)
+            ret = dict(vehicle=vehicle, added_users=added_adms, un_add_users=list(un_add_adms))
+        return render(request, 'oilWear/vehicle_adm.html', ret)
+
+
+    def post(self, request):
+        res = dict(result=False)
+        id_list = None
+        vehicle = get_object_or_404(Vehicle, pk=int(request.POST.get('id')))
+        if 'to' in request.POST and request.POST['to']:
+            id_list = map(int, request.POST.getlist('to', []))
+            id_list = list(id_list)
+        Operator.objects.filter(vehicle_id=vehicle.id).delete()
+        if id_list:
+            for user in User.objects.filter(id__in=id_list):
+                operator = Operator()
+                operator.operator = user
+                operator.vehicle = vehicle
+                operator.save()
+        res['result'] = True
+        return HttpResponse(json.dumps(res), content_type='application/json')
+
+
+class VehicleDeleteView(LoginRequiredMixin, View):
+    """
+    删除载具
+    """
+    def post(self, request):
+        res = dict()
+        vehicle = get_object_or_404(Vehicle, pk=int(request.POST.get('id')))
+        vehicle.is_delete = True
+        vehicle.save()
+        res['success'] = True
+        return HttpResponse(json.dumps(res), content_type='application/json')
